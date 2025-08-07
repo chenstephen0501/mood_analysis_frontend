f16e3530ed1740a4820746acc71a5c91cfa104ac
stephen0501 <tom9876555@gmail.com>
2025-08-07 11:38:02 +0800

    feat initial project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app-root">
+    <header>
+      <router-link :to="{ name: 'Home' }">Home</router-link> /
+      <router-link :to="{ name: 'About' }">About</router-link> /
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+
+<style scoped>
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>關於我們</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
